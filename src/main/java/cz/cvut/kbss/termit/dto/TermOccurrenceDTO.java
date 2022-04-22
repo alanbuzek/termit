20e6a918d87bf6d3be0fccacc9f67c60ff57cc67
Alan Buzek <alan.buzek@gmail.com>
2022-04-22 07:22:08 +0200

    implement create term occurrence from endpoint

---
+++ src/main/java/cz/cvut/kbss/termit/dto/TermOccurrenceDTO.java
@@ -0,0 +1,54 @@
+/**
+ * TermIt
+ * Copyright (C) 2019 Czech Technical University in Prague
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package cz.cvut.kbss.termit.dto;
+
+public class TermOccurrenceDTO {
+
+    private String selector;
+    private Integer start;
+    private String exactMatch;
+//    private
+
+
+    public TermOccurrenceDTO() {
+    }
+
+    public String getSelector() {
+        return selector;
+    }
+
+    public void setSelector(String selector) {
+        this.selector = selector;
+    }
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public String getExactMatch() {
+        return exactMatch;
+    }
+
+    public void setExactMatch(String exactMatch) {
+        this.exactMatch = exactMatch;
+    }
+}
--- src/main/java/cz/cvut/kbss/termit/rest/TermOccurrenceController.java
+++ src/main/java/cz/cvut/kbss/termit/rest/TermOccurrenceController.java
@@ -1,8 +1,15 @@
 package cz.cvut.kbss.termit.rest;
 
 import cz.cvut.kbss.jsonld.JsonLd;
+import cz.cvut.kbss.termit.dto.TermOccurrenceDTO;
 import cz.cvut.kbss.termit.model.assignment.TermWebsiteOccurrence;
+import cz.cvut.kbss.termit.model.assignment.WebsiteOccurrenceTarget;
 import cz.cvut.kbss.termit.model.resource.Resource;
+import cz.cvut.kbss.termit.model.resource.Website;
+import cz.cvut.kbss.termit.model.selector.CssSelector;
+import cz.cvut.kbss.termit.model.selector.Selector;
+import cz.cvut.kbss.termit.model.selector.TextPositionSelector;
+import cz.cvut.kbss.termit.model.selector.TextQuoteSelector;
 import cz.cvut.kbss.termit.security.SecurityConstants;
 import cz.cvut.kbss.termit.service.IdentifierResolver;
 import cz.cvut.kbss.termit.service.business.ResourceService;
@@ -11,6 +18,7 @@ import cz.cvut.kbss.termit.util.Configuration;
 import cz.cvut.kbss.termit.util.Constants;
 import cz.cvut.kbss.termit.util.CsvUtils;
 import cz.cvut.kbss.termit.util.Vocabulary;
+import org.apache.jena.atlas.json.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -19,6 +27,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -66,36 +75,34 @@ public class TermOccurrenceController extends BaseController {
         LOG.debug("Occurrence with identifier <{}> removed.", identifier);
     }
 
-    @PostMapping(consumes = JsonLd.MEDIA_TYPE)
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, JsonLd.MEDIA_TYPE})
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasRole('" + SecurityConstants.ROLE_FULL_USER + "')")
     public void createOccurrence(@RequestParam(name = Constants.QueryParams.NAMESPACE) String namespace,
                                  @RequestParam( required = false) Optional<String> termIdFragment,
                                  @RequestParam String websiteFragment,
-                                 @RequestBody TermWebsiteOccurrence termOccurrence
-//                      , @RequestBody Term term
+                                 @RequestBody TermOccurrenceDTO termOccurrenceDTO
+// TODO (alanb) uncomment this  @RequestBody TermWebsiteOccurrence termOccurrence
     ) {
-//        final URI websiteIdentifier =  idResolver.resolveIdentifier(namespace, websiteFragment);
-//        final Website website = (Website) resourceService.findRequired(websiteIdentifier);
-//        TermWebsiteOccurrence termWebsiteOccurrence = new TermWebsiteOccurrence();
-//        if (termIdFragment.isPresent()){
-//            final URI termUri = idResolver.resolveIdentifier(namespace, termIdFragment.get());
-//            termWebsiteOccurrence.setTerm(termUri);
-////          TODO: add correct types
-//        }
-//        WebsiteOccurrenceTarget websiteOccurrenceTarget = new WebsiteOccurrenceTarget(website);
-//        String containerCssSelector = "div > .example";
-//        String exactMatch = "example match";
-//        HashSet<Selector> selectors = new HashSet<>();
-//        selectors.add(new CssSelector(containerCssSelector));
-//        selectors.add(new TextQuoteSelector(exactMatch));
-//        selectors.add(new TextPositionSelector(5, 10));
-//        websiteOccurrenceTarget.setSelectors(selectors);
-//        termWebsiteOccurrence.setTarget(websiteOccurrenceTarget);
-        termOccurrence.addType(Vocabulary.s_c_vyskyt_termu);
-        occurrenceService.persist(termOccurrence);
+        final URI websiteIdentifier =  idResolver.resolveIdentifier(namespace, websiteFragment);
+        final Website website = (Website) resourceService.findRequired(websiteIdentifier);
+        TermWebsiteOccurrence termWebsiteOccurrence = new TermWebsiteOccurrence();
+        if (termIdFragment.isPresent()){
+            final URI termUri = idResolver.resolveIdentifier(namespace, termIdFragment.get());
+            termWebsiteOccurrence.setTerm(termUri);
+//          TODO: add correct types
+        }
+        WebsiteOccurrenceTarget websiteOccurrenceTarget = new WebsiteOccurrenceTarget(website);
+        HashSet<Selector> selectors = new HashSet<>();
+        selectors.add(new CssSelector(termOccurrenceDTO.getSelector()));
+        selectors.add(new TextQuoteSelector(termOccurrenceDTO.getExactMatch()));
+        selectors.add(new TextPositionSelector(termOccurrenceDTO.getStart(), -1));
+        websiteOccurrenceTarget.setSelectors(selectors);
+        termWebsiteOccurrence.setTarget(websiteOccurrenceTarget);
+        termWebsiteOccurrence.addType(Vocabulary.s_c_vyskyt_termu);
+        occurrenceService.persist(termWebsiteOccurrence);
 
-        LOG.debug("TermWebsiteOccurrence created: {}.", termOccurrence);
+        LOG.debug("TermWebsiteOccurrence created: {}.", termWebsiteOccurrence);
     }
 
 
